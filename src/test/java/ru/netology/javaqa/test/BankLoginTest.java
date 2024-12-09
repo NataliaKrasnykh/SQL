995384ab3ffc4894d320134f405345a92feaf7da
Natalia Krasnykh <krasnih.n161@mail.ru>
2024-12-09 23:07:52 +0300

    Initial commit

---
+++ src/test/java/ru/netology/javaqa/test/BankLoginTest.java
@@ -0,0 +1,56 @@
+package ru.netology.javaqa.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.javaqa.data.DataHelper;
+import ru.netology.javaqa.data.SQLHelper;
+import ru.netology.javaqa.page.DashboardPage;
+import ru.netology.javaqa.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.javaqa.data.SQLHelper.cleanAuthCodes;
+import static ru.netology.javaqa.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+    LoginPage loginPage;
+    DataHelper.AuthInfo authInfo = DataHelper.getAuthInfoWithTestData();
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    void ShouldSuccessfullyLogin() {
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void ShouldGetErrorNotificationLoginRandomUser() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.login(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    void shouldGetErrorNotificationRandomVerificationCode() {
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз.");
+    }
+}
